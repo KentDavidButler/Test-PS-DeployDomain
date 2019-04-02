df3253b726f8366857428b005a71439a9a8f983a
Kent Butler-HomeDesktop <KentDavidButler@gmail.com>
2019-04-02 09:09:51 -0400

    Initial Commit

---
+++ DeployDomain.ps1
@@ -0,0 +1,31 @@
+﻿if($hyperv.State -eq "Enabled")
+{
+    if(
+
+
+
+}
+Else
+{
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+}
+
+if(1 -eq 2)
+{
+#This section is to add bits of code that may be helpfull later
+
+#Comparrison opperators (-eq -ne  -gt  -ge  -lt  -le)
+#Comparrison Matching (-like  -notlike  -match  -notmatch (match and not match use regex pattern))
+#Containment           (-contains  -notcontains  -in  -notin)
+#replacement          (-replace (replaces a string pattern))
+#type                 ( -is  -isnot (checks if object are same type))
+
+
+
+#Commands
+#Get-WindowsFeatures (returns all availalbe features for machine as well as their PS name and if they are installed or not)
+
+#Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools  (install AD Domain services, good idea to check for admin account prior to this)
+
+
+}
