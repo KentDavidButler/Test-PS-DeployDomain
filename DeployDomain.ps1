aa2a0ad21772d34afd45d299754f22a842c3ec5d
Kent Butler-HomeDesktop <KentDavidButler@gmail.com>
2019-04-02 11:26:25 -0400

    getting availalble VMSwitch and passing that parameter to new VM

--- DeployDomain.ps1
+++ DeployDomain.ps1
@@ -1,5 +1,7 @@
 ﻿if($hyperv.State -eq "Enabled")
 {
+
+    #some point we'll need to check for virtual switch
     if(Get-VM -isnot null)
     {
         #check perameters of machine
@@ -7,7 +9,9 @@
     }
     else
     {
-
+        $NIC = Get-VMSwitch | Select-Object -First 1
+        $GetVMSwitchName = $NIC.Name
+        New-VM -Name TestWin10VM -MemoryStartupBytes 4GB -BootDevice VHD -NewVHDPath .\VMs\Win10.vhdx -Path .\VMData -NewVHDSizeBytes 20GB -Generation 2 -Switch $GetVMSwitchName
         #create new VM
         #need to determin what to create
         #a DC with domain? a simple win 10 os? remember babysteps over leaps and bounds
@@ -39,5 +43,8 @@ if(1 -eq 2)
 
 #Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools  (install AD Domain services, good idea to check for admin account prior to this)
 
+#Get-VMSwitch  * | Format-Table Name (gets list of virt switches)
 
+#Below command to create new VM, with name Win10Vm, that allocates 4GB to memeory and creates a new VirtHD and a path to where the HD exists on parent machine.
+#New-VM -Name Win10VM -MemoryStartupBytes 4GB -BootDevice VHD -NewVHDPath .\VMs\Win10.vhdx -Path .\VMData -NewVHDSizeBytes 20GB -Generation 2 -Switch ExternalSwitch  
 }
